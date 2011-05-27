bf506ee57a5b8997d10c8010d1c5d8c8e97763b1
Christoph Helma <helma@in-silico.ch>
2011-05-27 17:55:39 +0200

    test for bbrc rest parameters added

--- fminer.rb
+++ fminer.rb
@@ -32,6 +32,21 @@ class FminerTest < Test::Unit::TestCase
     d.delete(@@subjectid)
   end
 
+  def test_bbrc_rest_parameters
+    feature = @@classification_training_dataset.features.keys.first
+    #call = "curl -X POST #{File.join CONFIG[:services]["opentox-algorithm"],"fminer","bbrc"} -d \"dataset_uri=#{CGI.escape @@classification_training_dataset.uri}\" -d \"prediction_feature=#{CGI.escape feature}\" -d \"backbone=true\" -d \"min_frequency=2\""
+    dataset_uri = OpenTox::RestClientWrapper.post(File.join(CONFIG[:services]["opentox-algorithm"],"fminer","bbrc"),{
+      "dataset_uri" => @@classification_training_dataset.uri,
+      "prediction_feature" => feature,
+      "backbone" => true,
+      "min_frequency" => 2,
+       :subjectid => @@subjectid })
+    d =OpenTox::Dataset.new dataset_uri, @@subjectid
+    d.load_features(@@subjectid)
+    assert_equal 52, d.features.size
+
+  end
+
 # Deactivated by AM because of efficiency problems (does not return)
 #  def test_regression_last
 #    feature = File.join @@regression_training_dataset.uri,"feature/LC50_mmol" 
