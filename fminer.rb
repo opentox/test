983ad5cd9d1bc9f2ccd931b7d75fbb8b95e5f873
Christoph Helma <helma@in-silico.ch>
2011-06-17 15:21:14 +0200

    lazar dumpfiles implemented

--- fminer.rb
+++ fminer.rb
@@ -16,16 +16,16 @@ class FminerTest < Test::Unit::TestCase
     @dataset.delete(@@subjectid)
   end
 
-  def dump(method)
+  def dump
     @dataset = OpenTox::Dataset.find @dataset_uri, @@subjectid
-    @dumpfile = File.join(@dump_dir,method.to_s)+".yaml"
+    @dumpfile = File.join(@dump_dir,caller[0][/`.*'/][1..-2])+".yaml"
     File.open(@dumpfile,"w+"){|f| f.puts @dataset.to_yaml}
   end
 
   def test_bbrc
     feature = @@classification_training_dataset.features.keys.first
     @dataset_uri = OpenTox::Algorithm::Fminer::BBRC.new.run({:dataset_uri => @@classification_training_dataset.uri, :prediction_feature => feature, :subjectid => @@subjectid}).to_s
-    dump __method__
+    dump 
     assert_equal 52, @dataset.features.size
     cleanup
   end
@@ -33,7 +33,7 @@ class FminerTest < Test::Unit::TestCase
   def test_regression_bbrc
     feature = File.join @@regression_training_dataset.uri,"feature/LC50_mmol" 
     @dataset_uri = OpenTox::Algorithm::Fminer::BBRC.new.run({:dataset_uri => @@regression_training_dataset.uri, :prediction_feature => feature, :subjectid => @@subjectid, :feature_type=>"paths"}).to_s
-    dump __method__
+    dump
     assert_equal 219, @dataset.features.size
     cleanup
   end
@@ -41,7 +41,7 @@ class FminerTest < Test::Unit::TestCase
   def test_last
     feature = @@classification_training_dataset.features.keys.first
     @dataset_uri = OpenTox::Algorithm::Fminer::LAST.new.run({:dataset_uri => @@classification_training_dataset.uri, :prediction_feature => feature, :subjectid => @@subjectid}).to_s
-    dump __method__
+    dump
     assert_equal 23, @dataset.features.size
     cleanup
   end
@@ -54,7 +54,7 @@ class FminerTest < Test::Unit::TestCase
       "backbone" => true,
       "min_frequency" => 2,
        :subjectid => @@subjectid })
-    dump __method__
+    dump
     assert_equal 52, @dataset.features.size
     cleanup
   end
@@ -62,11 +62,10 @@ class FminerTest < Test::Unit::TestCase
 # Deactivated by AM because of efficiency problems (does not return)
 #  def test_regression_last
 #    feature = File.join @@regression_training_dataset.uri,"feature/LC50_mmol" 
-#    dataset_uri = OpenTox::Algorithm::Fminer::LAST.new.run({:dataset_uri => @@regression_training_dataset.uri, :prediction_feature => feature, :subjectid => @@subjectid}).to_s
-#    d =OpenTox::Dataset.new dataset_uri, @@subjectid
-#    d.load_features(@@subjectid)
+#    @dataset_uri = OpenTox::Algorithm::Fminer::LAST.new.run({:dataset_uri => @@regression_training_dataset.uri, :prediction_feature => feature, :subjectid => @@subjectid}).to_s
+#    dump
 #    assert_equal 4, d.features.size
-#    d.delete(@@subjectid)
+#    cleanup
 #  end
 
 end
