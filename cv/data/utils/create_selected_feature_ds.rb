76c11057ecdf0908a48b750c098293af47f298f0
davor <vorgrimmlerdavid@gmx.de>
2012-03-17 21:05:28 +0100

    Modified scripts.

--- cv/data/utils/create_selected_feature_ds.rb
+++ cv/data/utils/create_selected_feature_ds.rb
@@ -4,14 +4,14 @@ require 'yaml'
 
 @subjectid = nil
 
-
+@dataset = []
 
 def create_f_ds(t_ds_uri, f_ds_uri, del)
 
   regression_training_dataset = OpenTox::Dataset.find(t_ds_uri, @subjectid)
   prediction_feature = regression_training_dataset.features.keys.first
   regression_feature_dataset = OpenTox::Dataset.find(f_ds_uri, @subjectid)
-  
+
   params = {} 
   params[:dataset_uri] = regression_training_dataset.uri
   params[:prediction_feature_uri] = prediction_feature
@@ -20,12 +20,12 @@ def create_f_ds(t_ds_uri, f_ds_uri, del)
   puts params.to_yaml
   feature_selection_algo_uri = File.join(CONFIG[:services]["opentox-algorithm"],"feature_selection/rfe")
   puts feature_selection_algo_uri
+  puts "--- Feature dataset is: ---"
 
   result = OpenTox::RestClientWrapper.post( feature_selection_algo_uri, params)
-  puts "--- Feature dataset is: ---"
   puts result
-  
   puts
+  result
 end
 
 
@@ -34,15 +34,26 @@ end
 ds = YAML::load_file("../datasets.yaml")
 ds.keys.each { |dataset|
   puts "----------------- next dataset -----------------"
+  @dataset << "\"#{dataset}\": {" 
   ds[dataset].keys.each { |pc|
-    puts pc unless (pc == "dataset") || (pc == "test") || (pc == "training")
-    #[false, true].each { |del_missing| 
-    [false].each { |del_missing| #false is default 
-      begin
-        create_f_ds(ds[dataset]["dataset"], ds[dataset][pc], del_missing) unless (pc == "dataset") || (pc == "test") || (pc == "training")
-      rescue
-      end
-    }
+    if !((pc == "dataset") || (pc == "test") || (pc == "training") || (pc == "hybrid"))
+      puts pc
+      #[false, true].each { |del_missing| 
+      [false].each { |del_missing| #false is default 
+        begin
+          result = create_f_ds(ds[dataset]["dataset"], ds[dataset][pc], del_missing) 
+          @dataset << "  \"#{pc}\": \"#{result}\"," 
+        rescue
+        end
+      }
+    else
+      @dataset << "  \"#{pc}\": \"#{ds[dataset][pc]}\"," 
+
+    end
     puts "-----------------" unless pc == "dataset"
   }
+  @dataset << "}," 
 }
+
+puts @dataset
+
