fb8dd32a26824db7481d34b4ea38e1c710b15a3d
David Vorgrimmler <vorgrimmlerdavid@gmx.de>
2012-06-05 16:11:55 +0200

    Modified logging time

--- bbrc-sample/bbrc_sample.rb
+++ bbrc-sample/bbrc_sample.rb
@@ -16,7 +16,7 @@ path = ARGV[0]
 ds_file = path.split("/").last
 
 if File.exists?(path)
-  puts "[#{Time.now.utc.iso8601(4).to_s}] #{ds_file} exists."
+  puts "[#{Time.now.iso8601(4).to_s}] #{ds_file} exists."
 else
   puts "#{ds_file} does not exist."
   exit
@@ -25,7 +25,7 @@ end
 subjectid = nil
 
 ds_name = ARGV[1] # e.g. MOU
-num_boots = ARGV[2] # e.g. electronic,cpsa or nil to disable
+num_boots = ARGV[2] 
 
 ds = YAML::load_file("#{path}")
 ds_uri = ds[ds_name]["dataset"]
@@ -34,7 +34,7 @@ algo_params = {}
 algo_params["dataset_uri"] = ds_uri
 algo_params["num_boots"] = num_boots
 
-puts "[#{Time.now.utc.iso8601(4).to_s}] #{algo_params.to_yaml}"
+puts "[#{Time.now.iso8601(4).to_s}] #{algo_params.to_yaml}"
 
 ds = OpenTox::Dataset.find(ds_uri)
 ds_nr_de = ds.data_entries.size
@@ -57,12 +57,12 @@ n_stripped_cst = ds_result.metadata[OT::parameters][8][OT::paramValue]
 random_seed = ds_result.metadata[OT::parameters][9][OT::paramValue]
 
 puts
-puts "[#{Time.now.utc.iso8601(4).to_s}] Bbrc result: #{result_uri}"
-puts "[#{Time.now.utc.iso8601(4).to_s}] nr dataentries: #{ds_result_nr_de} , (of #{ds_nr_de} )"
-puts "[#{Time.now.utc.iso8601(4).to_s}] nr dataentries: #{ds_result_nr_com} , (of #{ds_nr_com} )"
-puts "[#{Time.now.utc.iso8601(4).to_s}] nr features: #{ds_result_nr_f}"
-puts "[#{Time.now.utc.iso8601(4).to_s}] Merge time: #{merge_time}"
-puts "[#{Time.now.utc.iso8601(4).to_s}] Duration: #{duration}"
+puts "[#{Time.now.iso8601(4).to_s}] Bbrc result: #{result_uri}"
+puts "[#{Time.now.iso8601(4).to_s}] nr dataentries: #{ds_result_nr_de} , (of #{ds_nr_de} )"
+puts "[#{Time.now.iso8601(4).to_s}] nr dataentries: #{ds_result_nr_com} , (of #{ds_nr_com} )"
+puts "[#{Time.now.iso8601(4).to_s}] nr features: #{ds_result_nr_f}"
+puts "[#{Time.now.iso8601(4).to_s}] Merge time: #{merge_time}"
+puts "[#{Time.now.iso8601(4).to_s}] Duration: #{duration}"
 
 puts "=hyperlink(\"#{ds_uri}\";\"#{ds_name}\"),#{num_boots},#{min_sampling_support},#{min_frequency_per_sample},#{nr_hits},=hyperlink(\"#{result_uri}\";\"bbrc_result\"),#{ds_result_nr_com},#{ds_nr_com},#{ds_result_nr_f},#{duration},#{merge_time},#{n_stripped_mss},#{n_stripped_cst},#{random_seed}"
 
